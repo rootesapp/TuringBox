4bc4816d9b105266d28f5434a1d8559836b1281e
rootesapp <htmlhtml5@foxmail.com>
2024-11-09 21:59:24 +0800

    Update MinePage.kt

--- app/src/main/java/io/github/lumyuan/turingbox/windows/main/MinePage.kt
+++ app/src/main/java/io/github/lumyuan/turingbox/windows/main/MinePage.kt
@@ -1,12 +1,12 @@
 package io.github.lumyuan.turingbox.windows.main
 
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import android.widget.Toast
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
@@ -58,10 +58,4 @@ fun MinePage() {
             Text(text = "文件功能")
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MinePage()
 }
