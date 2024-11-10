57f28c0b92876b9ea081c3b015bea15c30097ab3
rootesapp <htmlhtml5@foxmail.com>
2024-11-10 10:50:42 +0800

    Update FileSearchActivity.kt

--- app/src/main/java/io/github/lumyuan/turingbox/windows/main/activity/FileSearchActivity.kt
+++ app/src/main/java/io/github/lumyuan/turingbox/windows/main/activity/FileSearchActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.compose.rememberLauncherForActivityResult
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicTextField
