7a166546aac3ce3e07deb76f4c95e5f95d2dcf14
rootesapp <htmlhtml5@foxmail.com>
2024-11-10 10:12:44 +0800

    Update FunctionPage.kt

--- app/src/main/java/io/github/lumyuan/turingbox/windows/main/FunctionPage.kt
+++ app/src/main/java/io/github/lumyuan/turingbox/windows/main/FunctionPage.kt
@@ -100,21 +100,18 @@ fun FunctionPage() {
     }
 }
 
-// 自定义按钮，应用 MaterialTheme 配色方案
-// 自定义按钮，更具 iOS 风格
-// 自定义按钮，使用默认颜色
 @Composable
 fun CustomButton(buttonText: String, onClick: () -> Unit) {
     Button(
         onClick = onClick,
         modifier = Modifier
             .fillMaxWidth()  // 按钮宽度填满父容器
-            .padding(vertical = 8.dp) // 调整按钮的上下间距
+            .padding(vertical = 8.dp), // 调整按钮的上下间距
+        colors = ButtonDefaults.buttonColors() // 使用默认颜色
     ) {
         Text(text = buttonText)
     }
 }
-
 
 // 使用 OkHttp 获取公告内容
 suspend fun fetchAnnouncement(): String {
